ffcc601109cf424f9528cdb0f31b3a7a38701561
Carlos Coimbra <carlos.cccoimbra@hotmail.com>
2024-10-22 18:16:51 -0300

    adressed button id

--- Hello.py
+++ Hello.py
@@ -26,11 +26,11 @@ if not st.session_state.logged_in:
     col1, col2 = st.columns(2)
     
     with col1:
-        if st.button("Login"):
+        if st.button("Login", key="login_button"):
             st.session_state.page = "login"
     
     with col2:
-        if st.button("Register"):
+        if st.button("Register", key="register_button"):
             st.session_state.page = "register"
 
 # Handle Login and Registration
@@ -38,7 +38,7 @@ if st.session_state.get("page") == "login":
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     
-    if st.button("Login"):
+    if st.button("Login", key="login_submit"):
         if login_user(username, password):
             st.session_state.logged_in = True
             st.success("Login successful!")
@@ -50,7 +50,7 @@ elif st.session_state.get("page") == "register":
     new_username = st.text_input("Choose a username")
     new_password = st.text_input("Choose a password", type="password")
     
-    if st.button("Register"):
+    if st.button("Register", key="register_submit"):
         if register_user(new_username, new_password):
             st.success("Registration successful! You can now log in.")
             st.session_state.page = "login"  # Automatically switch back to login
@@ -63,7 +63,7 @@ if st.session_state.logged_in:
     st.write("Welcome to the Home Page!")
     st.write("You can access other pages in the 'pages' folder now.")
 
-    if st.button("Logout"):
+    if st.button("Logout", key="logout_button"):
         st.session_state.logged_in = False
         st.session_state.page = None  # Reset the page
         st.success("You have logged out.")
